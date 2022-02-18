2e8a24f292f0a30a7e7c06bf9c38dcdd9202e4c6
EcoTrend11 <bernardo_hl@outlook.com>
2022-02-18 17:52:13 -0500

    try resolve filter activity

--- api/src/routes/activities.js
+++ api/src/routes/activities.js
@@ -12,23 +12,47 @@ router.get('/',  async (req, res) =>{
 
 router.post('/addActivity', async (req, res)=>{
 
-
-
-
         const { name, level, duration, season} = req.body
         const countriId = req.body.countriId
-    
-        const createActivity= await Activity.create({
-            name : name,
-            level : level,
-            duration : duration,
-            season : season,
+
+        const search = countriId.map(async (e) => {
+          let country = await Country.findByPk(e)
+          // console.log(country)
+          
         })
-        console.log(countriId)
+        console.log(search)
+        res.send(search)
+
+
+
+        // if(countriId.length === 1){
+        //   const searchActivityCountry = await Country.findByPk(countriId[0],{
+        //     include : [{
+        //       model : Activity,
+        //       where: {name : name}
+        //     }]
+        //   })
+          
+        //   if(searchActivityCountry){
+        //     res.send(`el pais de id(s):${countriId[0]} ya tiene esa actividad`)
+        //   }else{
+
+        //   const createActivity= await Activity.create({
+        //       name : name,
+        //       level : level,
+        //       duration : duration,
+        //       season : season,
+        //   })
+                  
+        //   await createActivity.addCountry(toUpper(countriId[0]))
+        //   res.send(`Actividad ${name} creadad en el pais de id${countriId[0]}`)
+        //   }
+        // }
+        // if(countriId.length > 1){
+
         
-        
-        await createActivity.addCountry(toUpper(countriId[0]))
-        res.send("creado con exito")
+         
+
 
         
         
